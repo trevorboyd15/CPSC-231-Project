622f41cbc411c86ec115ed817466f87ab4239801
Trevor Boyd <32575497+trevorboyd15@users.noreply.github.com>
2018-01-22 13:34:52 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class Game {
     public static void main (String[] args){
-        System.out.print("Hello World!");   
+        System.out.print("Hello World!");
+        System.out.println("Cool stuff");
         
         
     }
