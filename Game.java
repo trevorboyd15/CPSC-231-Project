cdab659f2a60d9889f86d523a3740ebc51929452
Trevor Boyd <32575497+trevorboyd15@users.noreply.github.com>
2018-01-22 14:04:10 -0700

    Cool
    Stuff

--- Game.java
+++ Game.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class Game {
     public static void main (String[] args){
-        System.out.print("Hello World!");
-        System.out.println("Cool stuff");
-		System.out.println("Aidan is bad");
+        
         
         
     }
