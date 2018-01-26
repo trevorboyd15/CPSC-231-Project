5ee4e252ba9ae52dfb8ac8e642ccf7e4869b319a
trevorboyd15 <trevorboyd15@gmail.com>
2018-01-26 11:21:06 -0700

    Start of the Layout
    Just making updates

--- Game.java
+++ Game.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Game {
     public static void main (String[] args){
-        
+        Map m = new Map();
+		m.resetMap();
+		m.display();
         
         
     }
@@ -11,29 +13,88 @@ public class Game {
 }
 
 
-class character {
-	int posX;
-	int posY;
-	int health;
-	Item[] inventory;
+class Unit {
+
+	private int posX;
+	private int posY;
+	private int health;
+	private int armor;
+	private String name;
 	
-	void character(int h){
+	Unit(String n, int h, int a, int x, int y){
 		health = h;
-	
-	
-	}
-
-
-
-}
-
-class Item {
-	String name;
-
-	Item(String n){
 		name = n;
-	
+		armor = a;
+		posX = x;
+		posY = y;
 	}
 
 
+
 }
+
+class Building {
+
+	private String name;
+	private int posX;
+	private int posY;
+	
+	Building (String n, int x, int y){
+		name = n;
+		posX = x;
+		posY = y;
+	}
+}
+
+class Map {
+	private int size = 25;
+	private String[][] map = new String[size][size];
+	
+	void resetMap (){
+		for (int i = 0;i<this.size;i++){
+			for (int j = 0; j<this.size; j++){
+				this.map[i][j] = "--";
+			}
+		}
+	}
+	
+	void display(){
+		for (int i = 0;i<this.size;i++){
+			for (int j = 0; j<this.size; j++){
+				System.out.print(this.map[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
+
+class Resource {
+	
+	private String type;
+	private int amount;
+	private int posX;
+	private int posY;
+	private int mineAmount;
+
+
+}
+
+class GameState {
+	
+
+
+}
+
+class Player {
+	private String pNum;
+	private List <Unit> units;
+	private List <Building> buildings;
+
+
+
+
+}
+
+
+
+
