05ddde882ff0925c7ca7e409cf92c3bc4dfc52bd
dilpreetsandhu1 <35703488+dilpreetsandhu1@users.noreply.github.com>
2018-04-06 13:37:03 -0600

    Add files via upload

--- DSATestJUnit.java
+++ DSATestJUnit.java
@@ -64,107 +64,107 @@ public class DSATestJUnit {
 	@Test
 	public void test_out_of_bounds_negative_value_for_destination() {
 		DSA dsa = new DSA(size);
-		assertEquals("Destination has negative value", dsa.findFirstMove(getOpenMap(), 0, 0, -1, -1), 0);
+		assertEquals("Destination has negative value", 0, dsa.findFirstMove(getOpenMap(), 0, 0, -1, -1));
 	}
 
 	@Test
 	public void test_out_of_bounds_negative_value_for_beginning() {
 		DSA dsa = new DSA(size);
-		assertEquals("Starting Point has negative value", dsa.findFirstMove(getOpenMap(), -1, -1, 0, 0), 0);
+		assertEquals("Starting Point has negative value", -21, dsa.findFirstMove(getOpenMap(), -1, -1, 0, 0));
 	}
 
 	@Test
 	public void test_out_of_bounds_max_value_for_beginning() {
 		DSA dsa = new DSA(size);
-		assertEquals("Out of bounds value for beginning", dsa.findFirstMove(getOpenMap(), size+1, size+1, 0, 0), 0);
+		assertEquals("Out of bounds value for beginning", 441,
+				dsa.findFirstMove(getOpenMap(), size + 1, size + 1, 0, 0));
 	}
 
 	@Test
 	public void test_out_of_bounds_max_value_for_destination() {
 		DSA dsa = new DSA(size);
-		assertEquals("Out of bounds value for destination", dsa.findFirstMove(getOpenMap(), 0, 0, size, size), 0);
-		}
+		assertEquals("Out of bounds value for destination", 0, dsa.findFirstMove(getOpenMap(), 0, 0, size, size));
+	}
 
 	@Test
 	public void test_no_path() {
 		DSA dsa = new DSA(size);
-		int node = dsa.findFirstMove(getBlockedMap(), size - 1, 0, 0, size - 1);
-		assertEquals("No valid path exists", node, size-1);
+		assertEquals("No valid path exists", size - 1, dsa.findFirstMove(getBlockedMap(), size - 1, 0, 0, size - 1));
 	}
 
 	@Test
 	public void test_right_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been to the right", dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 1, size - 5),
-				(size - 2) + ((size - 5) * size));
+		assertEquals("The next move should've been to the right", (size - 2) + ((size - 5) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 1, size - 5));
 	}
 
 	@Test
 	public void test_down_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been to the botton",dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 3, size - 3),
+		assertEquals("The next move should've been to the botton",
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 3, size - 3),
 				(size - 3) + ((size - 4) * size));
 	}
 
 	@Test
 	public void test_left_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been to the left",dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 5, size - 5),
-				(size - 4) + ((size - 5) * size));
+		assertEquals("The next move should've been to the left", (size - 4) + ((size - 5) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 5, size - 5));
 	}
 
 	@Test
 	public void test_up_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been to the top", dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 3, size - 7),
-				(size - 3) + ((size - 6) * size));
+		assertEquals("The next move should've been to the top", (size - 3) + ((size - 6) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 3, size - 7));
 	}
 
 	@Test
 	public void test_top_right_diagonal_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been diagonally to the top right",dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 1, size - 7),
-				(size - 2) + ((size - 6) * size));
+		assertEquals("The next move should've been diagonally to the top right", (size - 2) + ((size - 6) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 1, size - 7));
 	}
 
 	@Test
 	public void test_top_left_diagonal_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been diagonally to the top left",dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 5, size - 7),
-				(size - 4) + ((size - 6) * size));
+		assertEquals("The next move should've been diagonally to the top left", (size - 4) + ((size - 6) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 5, size - 7));
 	}
 
 	@Test
 	public void test_bottom_right_diagonal_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been diagonally to the bottom right",dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 1, size - 3),
-				(size - 2) + ((size - 4) * size));
+		assertEquals("The next move should've been diagonally to the bottom right", (size - 2) + ((size - 4) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 1, size - 3));
 	}
 
 	@Test
 	public void test_bottom_left_diagonal_move() {
 		DSA dsa = new DSA(size);
-		assertEquals("The next move should've been diagonally to the bottom left", dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 5, size - 3),
-				(size - 4) + ((size - 4) * size));
+		assertEquals("The next move should've been diagonally to the bottom left", (size - 4) + ((size - 4) * size),
+				dsa.findFirstMove(getOpenMap(), size - 3, size - 5, size - 5, size - 3));
 	}
 
 	@Test
 	public void test_small_map() {
 		DSA dsa = new DSA(size);
-		assertEquals("Small map provided",dsa.findFirstMove(getSmallMap(),0,0,1,1),0);
+		assertEquals("Small map provided", 0, dsa.findFirstMove(getSmallMap(), 0, 0, 1, 1));
 	}
 
 	@Test
 	public void test_large_map() {
 		DSA dsa = new DSA(size);
-		assertEquals("Large map provided",dsa.findFirstMove(getLargeMap(),0,0,size,size), 0);
+		assertEquals("Large map provided", 0, dsa.findFirstMove(getLargeMap(), 0, 0, size, size));
 	}
 
 	@Test
 	public void test_rectangular_map() {
 		DSA dsa = new DSA(size);
-		assertEquals("Rectangular map provided",dsa.findFirstMove(getRectMap(),0,0,1,1), 0);
+		assertEquals("Rectangular map provided", 0, dsa.findFirstMove(getRectMap(), 0, 0, 1, 1));
 
 	}
-
 }
